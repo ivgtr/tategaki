ed9af46f09059a108330cf81789ec73eb7257b8b
ivgtr <ivgtr.me@gmail.com>
2024-07-08 18:54:00 +0900

    ワークフロー修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // baseプロパティに設定する値
+  let base = "/";
+
+  // 本番時に適用させるbaseの値
+  if (mode === "production") {
+    base = "/tategaki/";
+  }
+
+  return {
+    plugins: [react()],
+    // baseプロパティをbase変数で指定
+    base: base,
+  };
+});
